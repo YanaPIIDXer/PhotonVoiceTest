0398ebe0308e9c9e55530d902693403d1cab8c90
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-30 03:16:48 +0900

    比較用コードはこっちではナンセンスなので撤去

--- Assets/Scripts/Flow/MatchMakeFlow.cs
+++ Assets/Scripts/Flow/MatchMakeFlow.cs
@@ -5,6 +5,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.SceneManagement;
 using Pun2Task;
+using Photon.Voice.PUN;
 
 namespace Game.Flow
 {
@@ -46,79 +47,5 @@ namespace Game.Flow
                 Debug.LogError(e.Message);
             }
         }
-
-        // ↓本当はこんな残し方しちゃダメ
-        //  Pun2Taskとの比較用にあえて残してある
-#if false
-        public override void OnDisconnected(DisconnectCause cause)
-        {
-            if (cause != DisconnectCause.DisconnectByClientLogic)       // ←PlayModeを終了した時もコレが飛んでくる
-            {
-                Debug.LogError("Disconnected... Reason:" + cause.ToString());
-            }
-        }
-
-        public override void OnConnectedToMaster()
-        {
-            Debug.Log("On Connected Server!");
-            PhotonNetwork.JoinLobby();
-        }
-
-        public override void OnJoinedLobby()
-        {
-            Debug.Log("On Joined Lobby!");
-            Debug.Log("RoomCount:" + PhotonNetwork.CountOfRooms);
-            if (PhotonNetwork.CountOfRooms == 0)
-            {
-                SceneManager.LoadScene("Game");
-                PhotonNetwork.CreateRoom("TestRoom");
-            }
-        }
-
-        public override void OnRoomListUpdate(List<RoomInfo> roomList)
-        {
-            Debug.Log("OnUpdateRoomList Count:" + roomList.Count);
-            RoomInfo Join = null;
-            foreach (var Room in roomList)
-            {
-                Debug.Log(Room.Name);
-                if (Join == null)
-                {
-                    // とりあえず最初の部屋に突っ込む
-                    Join = Room;
-                }
-            }
-
-            if (Join != null)
-            {
-                SceneManager.LoadScene("Game");
-                PhotonNetwork.JoinRoom(Join.Name);
-            }
-        }
-
-        public override void OnCreatedRoom()
-        {
-            Debug.Log("Room Create Success!");
-            Debug.Log("Name:" + PhotonNetwork.CurrentRoom.Name);
-            // ↓注意：OnCreatedRoomの直後、勝手にOnJoinedRoomが呼ばれるフロー
-            //SceneManager.LoadScene("Game");
-        }
-
-        public override void OnCreateRoomFailed(short returnCode, string message)
-        {
-            Debug.LogError("Create Room Failed. Message:" + message);
-        }
-
-        public override void OnJoinedRoom()
-        {
-            Debug.Log("Room Join Success!");
-            Debug.Log("Name:" + PhotonNetwork.CurrentRoom.Name);
-        }
-
-        public override void OnJoinRoomFailed(short returnCode, string message)
-        {
-            Debug.LogError("Join Room Failed. Message:" + message);
-        }
-#endif
     }
 }
